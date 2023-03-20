c17ec4c4fe28f0b0b80f65ce96f683b9a0474a1d
ManeeshaThotakura <2100039089cse.r@gmail.com>
2023-03-20 12:08:32 +0530

    create user

--- Lakshmihospital/src/Main.java
+++ Lakshmihospital/src/Main.java
@@ -65,6 +65,11 @@ public class Main extends javax.swing.JFrame {
         jButton6.setText("Create item");
 
         jButton7.setText("Create user");
+        jButton7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton7ActionPerformed(evt);
+            }
+        });
 
         jButton8.setText("View Doctor");
 
@@ -203,6 +208,13 @@ public class Main extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
+                // TODO add your handling code here:
+                User u=new User();
+                u.setVisible(true);
+                
+    }//GEN-LAST:event_jButton7ActionPerformed
 
     /**
      * @param args the command line arguments
